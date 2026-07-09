2141cc224ff6c0dc116ab331727ab31bc1a64da4
MSU-39 <f0106782@msu.edu>
2026-07-09 10:57:10 -0400

    shift partially working
    fix the partial material color thingy issue idk

--- Assets/Scripts/colorState.cs
+++ Assets/Scripts/colorState.cs
@@ -1,10 +1,19 @@
+using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class colorState : MonoBehaviour
 {
-    public ColorManager.StandardColor color;
+
     public Renderer colorRenderer;
+
     public bool colorShift = true;
+    public bool isBlue = false;
+    public bool enableCollision = false;
+    public Collider collisions;
+
+    InputAction shiftColors;
+
     // when colorShift = true, it is on redShift, and when colorShift = false, it is on blueShift
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -12,9 +21,15 @@ public class colorState : MonoBehaviour
     void Start()
 
     {
+        shiftColors = InputSystem.actions.FindAction("Shift");
+
         colorRenderer = GetComponentInChildren<Renderer>();
 
-       // colorRenderer.material = ColorManager.singleton.standardMattes[(int)color];
+        //collision thingy woah
+
+        collisions = GetComponentInChildren<Collider>();
+
+       //colorRenderer.material = ColorManager.singleton.standardMattes[(int)color]; <- useless garbage meh meh meh 
     }
     
 
@@ -22,17 +37,48 @@ public class colorState : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Collision updater
+
+        collisions.enabled = enableCollision;
+
+
+        if (shiftColors.WasCompletedThisFrame())
+        {
+            colorShift = !colorShift;
+        }
+        
 
         if (colorShift)
         {
-            colorRenderer.material.SetColor("_BaseColor", new Color(0f,0f,1f,0.25f));
-            colorRenderer.material.SetColor("_EmissionColor", new Color(0f, 0f, 1f, 0.25f));
-
+            if (isBlue)
+            {
+                //blue transparent stuff
+                colorRenderer.material.SetColor("_BaseColor", new Color(0f, 0f, 1f, 0.25f));
+                colorRenderer.material.SetColor("_EmissionColor", new Color(0f, 0f, 1f, 0.25f));
+                enableCollision = false;
+            } else
+            {
+                //red solid stuff
+                colorRenderer.material.SetColor("_BaseColor", Color.red);
+                colorRenderer.material.SetColor("_EmissionColor", Color.red);
+                enableCollision = true;
+            }
         }
         else
         {
-            colorRenderer.material.SetColor("_BaseColor", Color.blue);
-            colorRenderer.material.SetColor("_EmissionColor", Color.blue);
+            if(isBlue)
+            {
+                //blue solid stuff
+                colorRenderer.material.SetColor("_BaseColor", Color.blue);
+                colorRenderer.material.SetColor("_EmissionColor", Color.blue);
+                enableCollision = true;
+            } else
+            {
+                //red transparent stuff
+                colorRenderer.material.SetColor("_BaseColor", new Color(1f, 0f, 0f, 0.25f));
+                colorRenderer.material.SetColor("_EmissionColor", new Color(1f, 0f, 0f, 0.25f));
+                enableCollision = false;
+            }
         }
     }
 }
